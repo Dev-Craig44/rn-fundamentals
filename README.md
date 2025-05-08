dca27b400160fbbb49627adcb4320a1932cb06a6
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-05-08 16:14:53 -0400

    🧠 use borders

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # rn-fundamentals
 
 A hands-on journey through React Native fundamentals, packed with practical examples, custom scripts, and real-world insights. Build with confidence.
+
+# Styling
+
+# Borders
+
+To summarize the section on borders:
+
+- **Border Width**: Controls the thickness of the border.
+- **Border Color**: Sets the color of the border.
+- **Border Radius**: Rounds the corners of the border.
+- **Border Style**: Defines the style (e.g., solid, dashed, dotted).
+
+Example:
+
+```javascript
+const styles = StyleSheet.create({
+  box: {
+    borderWidth: 2,
+    borderColor: "blue",
+    borderRadius: 10,
+    borderStyle: "solid",
+  },
+});
+```
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,30 @@
 import { View } from "react-native";
-import ViewImageScreen from "./screens/ViewImageScreen";
 
 export default function Index() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          // To create a circle, set the borderRadius to half the width/height of the object
+          height: 100,
+          // Add a border
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50,
+          // Uncomment to make the top border thicker
+          // borderTopWidth: 20,
+          // Uncomment to adjust the top-left corner radius
+          // borderTopLeftRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
