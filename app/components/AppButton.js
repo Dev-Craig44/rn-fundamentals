f9ba11e6f1e8c6274e9682ae23ac1950e779e03b
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-05-14 11:32:25 -0400

    🧼 refactor welcome screen buttons

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -6,10 +6,15 @@ import colors from "../config/colors";
 // use "rsf" to create a button component
 // give it a [title] prop for consumer to put place
 // we also want to be able to handle the [OnPress] event from outside the component
-function AppButton({ title, onPress }) {
+function AppButton({ title, onPress, color = "primary" }) {
+  // we want to be able to change the color of the button based on the prop passed
   return (
     // to make this button touchable we need to wrap eveything in a 'TouchableOpacity' component
-    <TouchableOpacity style={styles.button} onPress={onPress}>
+    <TouchableOpacity
+      // Since the default color is set to "primary" in the parameters, we can dynamically assign the background color by using the color parameter within the colors object.
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
       {/* inside this view we will have a text component */}
       <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
@@ -27,9 +32,10 @@ const styles = StyleSheet.create({
     padding: 15,
     // button should stretch to fill its container
     width: "100%",
+    marginVertical: 10,
   },
   text: {
-    colors: colors.white,
+    color: colors.white,
     fontSize: 18,
     textTransform: "uppercase",
     fontWeight: "bold",
--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
-import LoginButton from "../components/LoginButton";
-import RegisterButton from "../components/RegisterButton";
+import AppButton from "../components/AppButton";
 
 function WelcomeScreen() {
   return (
@@ -15,13 +14,16 @@ function WelcomeScreen() {
           style={styles.logo}
           source={require("../assets/images/logo-red.png")}
         />
-        <Text style={styles.text}>Sell What You Dont Need</Text>
+        <Text style={styles.tagline}>Sell What You Dont Need</Text>
       </View>
-      <LoginButton title={"Login"} onPress={() => console.log("logged in")} />
-      <RegisterButton
-        title={"Register"}
-        onPress={() => console.log("registered")}
-      />
+      <View style={styles.buttonsContainer}>
+        <AppButton title="Login" onPress={() => console.log("Logged in")} />
+        <AppButton
+          title="Register"
+          onPress={() => console.log("Registered")}
+          color="secondary"
+        />
+      </View>
     </ImageBackground>
   );
 }
@@ -32,19 +34,23 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     alignItems: "center",
   },
+  buttonsContainer: {
+    width: "100%",
+    padding: 20,
+  },
   logo: {
     width: 100,
     height: 100,
-    marginBottom: 20,
   },
   loginContainer: {
     position: "absolute",
     top: 70,
     alignItems: "center",
   },
-  text: {
-    fontSize: 18,
-    fontWeight: "bold",
+  tagline: {
+    fontSize: 25,
+    fontWeight: "600",
+    paddingVertical: 20,
   },
 });
 
