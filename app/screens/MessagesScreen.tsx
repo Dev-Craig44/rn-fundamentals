59333bc442d01528c66b2e849862c588fa4d4809
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-08 15:26:13 -0500

    🧼

--- app/screens/MessagesScreen.tsx
+++ app/screens/MessagesScreen.tsx
@@ -1,7 +1,5 @@
-// 1.) Type `rsf` to create a React Functional Component
 import React from "react";
 import { FlatList } from "react-native";
-// 5.) Import the ListItem component.
 import ListItem from "../components/ListItem";
 
 const messages = [
@@ -21,12 +19,9 @@ const messages = [
 
 function MessagesScreen(props: {}) {
   return (
-    // 2.) Render a flatlist.
     <FlatList
       data={messages}
-      // 3.) Provide a unique key for each item.
       keyExtractor={(message) => message.id.toString()}
-      // 4.) Render each item.
       renderItem={({ item }) => (
         <ListItem
           title={item.title}
