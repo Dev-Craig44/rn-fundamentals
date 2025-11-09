7fb2bda1e9b63c34bda01a8ea61c04bc62079afd
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-08 20:11:28 -0500

    🧼

--- app/screens/MessagesScreen.tsx
+++ app/screens/MessagesScreen.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-// 2.) Import StyleSheet, Platform, and StatusBar from react-native so we can adjust for the notch on Android devices
 import { FlatList, StyleSheet } from "react-native";
 import ListItem from "../components/ListItem";
-// 5.) Import Constants from expo-constants to get the status bar height
-// 6.) import Screen from "../components/Screen";
 import Screen from "../components/Screen";
 
 const messages = [
@@ -23,9 +20,6 @@ const messages = [
 
 function MessagesScreen(props: {}) {
   return (
-    // 1.) Add SafeAreaView in the root component of the screen
-    // 4.) Apply the styles object to the SafeAreaView component
-    // 7.) Remove SafeAreaView and wrap the FlatList with the Screen component
     <Screen>
       <FlatList
         data={messages}
@@ -42,12 +36,8 @@ function MessagesScreen(props: {}) {
   );
 }
 
-// 3.) Create a styles object to adjust for the notch on Android devices by typing `rnss`
 const styles = StyleSheet.create({
-  screen: {
-    // 6.) Add paddingTop using Constants.statusBarHeight to adjust for the notch on Android devices
-    // paddingTop: Constants.statusBarHeight,
-  },
+  screen: {},
 });
 
 export default MessagesScreen;
