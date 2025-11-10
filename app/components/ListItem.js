5f0c4eb281c1bc55c5efb540410cfca5e787b03c
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-10 02:31:13 -0500

    🧼

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { Image, StyleSheet, TouchableHighlight, View } from "react-native";
+import Swipeable from "react-native-gesture-handler/Swipeable";
 import colors from "../config/colors";
 import AppText from "./AppText";
-// 1.) import the swipeable component
-import Swipeable from "react-native-gesture-handler/Swipeable";
 
-// 4.) Add the renderRightActions prop to the ListItem component so that it can be customized by the consumer
 function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
   return (
-    // 2.) wrap the entire ListItem in the Swipeable component
-    // 3.) Add the renderRightActions prop and provide a function that returns the actions
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight onPress={onPress} underlayColor={colors.light}>
         <View style={styles.container}>
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,11 @@
 import { Stack } from "expo-router";
 import React from "react";
-import MessagesScreen from "./screens/MessagesScreen";
-// 5.) import the gesture handler root view
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import MessagesScreen from "./screens/MessagesScreen";
 
 export default function Index() {
   return (
     <>
-      {/* 6.) wrap the entire app in the GestureHandlerRootView */}
       <GestureHandlerRootView>
         <Stack.Screen options={{ headerShown: false }} />
         <MessagesScreen />
