0fb879bcefaa9f7c3300b2cb24e9acb89f019e46
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-12 12:54:58 -0500

    🧼

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -4,7 +4,6 @@ import Swipeable from "react-native-gesture-handler/Swipeable";
 import colors from "../config/colors";
 import AppText from "./AppText";
 
-// 2.) Add a ImageComponent prop to ListItem
 function ListItem({
   title,
   subTitle,
@@ -17,14 +16,10 @@ function ListItem({
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight onPress={onPress} underlayColor={colors.light}>
         <View style={styles.container}>
-          {/* 3.) Render the ImageComponent if it exists. This is so we can pass in any custom image component */}
           {ImageComponent}
-          {/* 1.) Wrap image in a conditional so that if we don't have an image prop, we don't render the Image component */}
           {image && <Image source={image} style={styles.image} />}
-          {/* 4.) Give detailsContainer a style */}
           <View style={styles.detailsContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            {/* 5.) Wrap subTitle in a conditional so that if we don't have a subTitle prop, we don't render the AppText component */}
             {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
