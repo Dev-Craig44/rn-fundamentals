ea6aa8584fc4d0b337cba7746dc70d00cb3eeec2
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-13 21:28:20 -0500

    🧼

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
-import { Switch } from "react-native";
+import React from "react";
 import Screen from "./components/Screen";
 
 export default function Index() {
-  // 2. set up state for the Switch
-  const [isNew, setIsNew] = useState(false);
-
   return (
     <>
-      <Screen style={undefined}>
-        {/* 1.) render a Switch */}
-        {/* 3.) set the value and onValueChange props */}
-        <Switch
-          value={isNew}
-          onValueChange={(newValue) => setIsNew(newValue)}
-        />
-      </Screen>
+      <Screen style={undefined}></Screen>
     </>
   );
 }
