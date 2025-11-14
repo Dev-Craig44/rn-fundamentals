7d83455f81fd40cae4167ab6eb5833795ec38467
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-13 19:13:55 -0500

    🧼

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,10 @@
-// 4.) import useState from react
-import React, { useState } from "react";
-import { StyleSheet, Text, TextInput } from "react-native";
+import React from "react";
 import Screen from "./components/Screen";
 
 export default function Index() {
-  // 5.) create state variable for first name
-  const [firstName, setFirstName] = useState("");
-
   return (
     <>
-      <Screen>
-        <Text>{firstName}</Text>
-        {/* 1.) render TextInput */}
-        <TextInput
-          // 6.) set onChangeText to update state variable
-          onChangeText={(text) => setFirstName(text)}
-          value={firstName}
-          placeholder="First Name"
-          style={styles.input}
-          // 3.) make placeholder text a bit gray
-          placeholderTextColor={"#6e6969"}
-        />
-      </Screen>
+      <Screen></Screen>
     </>
   );
 }
-
-// 2.) define styles for container and input | `RNSS`
-const styles = StyleSheet.create({
-  input: {
-    borderBottomColor: "#ccc",
-    borderBottomWidth: 1,
-  },
-});
