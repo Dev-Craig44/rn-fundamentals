3615ed5357ca04d9ba31006f703e9979f2857a62
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-11-13 19:46:04 -0500

    🧼

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-import AppTextInput from "./components/AppTextInput";
 import Screen from "./components/Screen";
-import colors from "./config/colors";
 
 export default function Index() {
   return (
     <>
-      <Screen>
-        {/* nothing happens because we are not applying this prop to our TextInput. */}
-        <AppTextInput
-          placeholder="Username"
-          placeholderTextColor={colors.medium}
-          // 23.) set the icon prop to "email"
-          icon="email"
-        />
-      </Screen>
+      <Screen></Screen>
     </>
   );
 }
